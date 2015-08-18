ba5f1e501250acc69dfc236b869706c1f61f34ff
Shivam Prakash <sp160194@gmail.com>
2015-08-18 16:53:39 +0530

    new variable added

--- Demo1/src/mainpack/MainApp.java
+++ Demo1/src/mainpack/MainApp.java
@@ -2,7 +2,8 @@ package mainpack;
 
 public class MainApp {
 	public static void main(String[] args) {
-		system.out.println("Hello Sapient\n");
+		int num1;
+		System.out.println("Hello Sapient\n");
 	}
 		
 }
